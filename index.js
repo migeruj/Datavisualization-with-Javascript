c266b5b542b9e44c2ad49974839f94adc398a933
migeruj <migeruj@gmail.com>
2022-11-23 21:53:40 -0500

    Ag3 Included

--- index.js
+++ index.js
@@ -1,2 +1,45 @@
 // Import stylesheets
 import './style.css';
+
+d3plus
+  .viz()
+  .container('#viz1')
+  .data([
+    { year: 1991, name: 'alpha', value: 15 },
+    { year: 1992, name: 'alpha', value: 20 },
+  ])
+  .type('bar')
+  .id('name')
+  .x('year')
+  .y('value')
+  .draw();
+
+d3.json(
+  'https://raw.githubusercontent.com/raul27868/07MBIG-Visualizacion-Actividades-Guiadas/master/columnas.json',
+  function (data) {
+    d3plus
+      .viz()
+      .container('#viz2')
+      .data(data)
+      .type('bar')
+      .id('name')
+      .x('year')
+      .y('value')
+      .draw();
+  }
+);
+
+d3.json(
+  'https://raw.githubusercontent.com/raul27868/07MBIG-Visualizacion-Actividades-Guiadas/master/columnas.json',
+  function (data) {
+    d3plus
+      .viz()
+      .container('#viz3')
+      .data(data)
+      .type('box')
+      .id('name')
+      .x('year')
+      .y('value')
+      .draw();
+  }
+);
